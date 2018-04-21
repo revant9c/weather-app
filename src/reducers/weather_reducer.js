43a22988e945a99934f7747fd5bad2f95d91d131
revant <revant@reflexionsdata.com>
2018-04-20 21:51:13 -0700

    Weather app to find weather of a particluar city

---
+++ src/reducers/weather_reducer.js
@@ -0,0 +1,11 @@
+//never mutate the existing state, always return a new state
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            //return state.concat([action.payload.data]);
+            return [action.payload.data, ...state]; //es6 syntax
+    }
+    return state;
+}
